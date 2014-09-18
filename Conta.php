c7b7fdb5cd05be9113d50bda94bb92b7a291536f
Ana Correia <acscorreia@hotmail.com>
2014-09-18 12:43:26 -0300

    Orientação a Objetos com PHP - Iniciando

---
+++ Conta.php
@@ -0,0 +1,38 @@
+<?php 
+/*
+  Orientação a Objetos com PHP - Módulo Banking Iniciando
+  Author: Ana Correia
+  Date: 18/09/2014
+  Mail: anacorreia.dev@gmail.com
+*/
+
+Class Conta
+{
+  protected $saldo;
+   
+  public function depositar ($valor)
+  {
+    $this->saldo += $valor;
+  }
+    
+  public function sacar ($valor)
+  {
+    if ($this->saldo  >=  $valor)
+    {
+      $this->saldo -= $valor;
+      return true;
+    }
+      return false;
+  }
+
+  public function getSaldo()
+  {
+    return $this->saldo;
+  }
+}
+
+$conta = new Conta();
+$conta->depositar(10000);
+$conta->sacar(1000);
+echo $conta->getSaldo();
+?>
